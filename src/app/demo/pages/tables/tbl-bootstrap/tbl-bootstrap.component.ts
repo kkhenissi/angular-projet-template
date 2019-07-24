abd5bb31e3858207a706eb28c2f0a2d7b5409fcc
kkhenissi <kkhenissi@yahoo.fr>
2019-07-24 06:04:14 +0200

    first commit

---
+++ src/app/demo/pages/tables/tbl-bootstrap/tbl-bootstrap.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+// import { setInterval } from 'timers';
+
+@Component({
+  selector: 'app-tbl-bootstrap',
+  templateUrl: './tbl-bootstrap.component.html',
+  styleUrls: ['./tbl-bootstrap.component.scss']
+})
+export class TblBootstrapComponent implements OnInit {
+  urlPhoto: string;
+  isActive: boolean;
+  test: boolean = true;
+
+  counter: number;
+  @ViewChild('myInput') myInput: ElementRef;
+  
+  constructor() { }
+
+  ngOnInit() {
+    this.urlPhoto = '../../../../assets/images/nav-bg/navbar-img-2.jpg';
+    this.counter = 5;
+    this.isActive = true;
+  }
+
+  myInputFunc() {
+    console.log('6666666666666666666666666')
+  }
+
+  sendFunction() {
+    console.log('7777777777777777777777777',this.myInput.nativeElement.value)
+  }
+
+}
